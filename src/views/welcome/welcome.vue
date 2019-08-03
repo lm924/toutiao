f38013872023c2fbc084981d2dd13a7a3eb1397b
lming <1592233187@qq.com>
2019-08-03 14:37:11 +0800

    home

---
+++ src/views/welcome/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <dir class="container" style="text-align:center">
+ <img src="../../assets/images/welcome.jpg" alt="">
+    </dir>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
